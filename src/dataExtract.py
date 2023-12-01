daca499451643d4cfa5874d68701066471406126
kevniu95 <kniu@uchicago.edu>
2023-11-30 21:42:34 -0600

    Re-organize dataExtract module into save, load, export, and finalize families of functions

--- src/dataExtract.py
+++ src/dataExtract.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 from .dataImport import initYearDict
 import pandas as pd
-from typing import Dict, Any
+from typing import Dict, Any, Set
 from util.constants import *
 from util.logger_config import setup_logger
 import re
@@ -10,6 +10,9 @@ import glob
 
 logger = setup_logger(__name__)
 
+# ===========
+# Extract and save functions
+# ===========
 def getConcatDateFrame(yearDict : Dict[Any, Any], dataSource : DataSource, dfsSite : DfsSite) -> pd.DataFrame:
     """
     From yearDict, extract the dataframe for the given dataSource and dfsSite, 
@@ -34,13 +37,32 @@ def extractFramesByDataSource(yearDict : Dict[Any, Any], dfsSite : DfsSite) -> D
     return frames
 
 def extractAndSaveYear(year : int) -> None:
+    """
+    'Extract' from pickle saved in dataImport module
+    'Save' all dates into one csv for each year in data/import folder
+    """
     yearDict = initYearDict(year)
     logger.info(f"Extracting data for {year}...")
     frames = extractFramesByDataSource(yearDict, DfsSite.DRAFTKINGS) # key is DataSource
     logger.info(f"Saving data for {year}...")
     for key, v in frames.items():
         v.to_csv(f'../data/import/{key}_{year}.csv', index=False)
-        
+
+# ===========
+# Load Functions
+# ===========
+# Note save happens on conditional basis within load functions
+def loadFrames(target_years : Set[int]) -> Dict[DataSource, pd.DataFrame]:
+    frames = {}
+    for dataSource in DataSource:
+        saved_years = {int(re.search(r'_(\d{4})\.csv', i).group(1)) for i in glob.glob(f'../data/import/{dataSource.value}_*.csv')}
+        missing_years = target_years - saved_years
+        for year in missing_years:
+            logger.info(f"Extracting and saving out data for {year}...")
+            extractAndSaveYear(year)
+        frames[dataSource.value] = loadSource(dataSource)
+    return frames
+
 def loadSource(dataSource : DataSource) -> pd.DataFrame:
     frames = {}
     for i in glob.glob(f'../data/import/{dataSource.value}_*.csv'):
@@ -49,12 +71,6 @@ def loadSource(dataSource : DataSource) -> pd.DataFrame:
         df['Season'] = year
         frames[year] = df
     return pd.concat(frames.values())
-
-def exportToMakeLookup(df : pd.DataFrame, name_field : str, team_field : str, dataSource : DataSource) -> None:
-    names = df[[name_field, team_field]].drop_duplicates()
-    names[['FirstName', 'LastName']] = names[name_field].str.split(' ', expand=True)
-    names.sort_values(by=['LastName','FirstName', team_field], inplace=True)
-    names.to_csv(f'../data/lookup/names_{dataSource.value}.csv', index=False)
     
 def standardizeNames(dfs : pd.DataFrame, ref : pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
     dfs['Name'] = dfs['Name'].str.replace(r' [A-Z]$', '', regex=True)
@@ -66,75 +82,63 @@ def standardizeNames(dfs : pd.DataFrame, ref : pd.DataFrame) -> (pd.DataFrame, p
     ref.rename(columns={'Player' : 'Name', 'Tm' : 'Team'}, inplace=True)
     return dfs, ref
 
+# ===========
+# Export functions
+# ===========
+def exportToMakeLookup(df : pd.DataFrame, name_field : str, team_field : str, dataSource : DataSource) -> None:
+    names = df[[name_field, team_field]].drop_duplicates()
+    names[['FirstName', 'LastName']] = names[name_field].str.split(' ', expand=True)
+    names.sort_values(by=['LastName','FirstName', team_field], inplace=True)
+    names.to_csv(f'../data/lookup/names_{dataSource.value}.csv', index=False)
+
+def exportLookupHelpers(frames : Dict[DataSource, pd.DataFrame]) -> None:
+    for dataSource in DataSource:
+        if not os.path.exists(f'../data/lookup/names_{dataSource.value}.csv'):
+            exportToMakeLookup(frames[dataSource.value], 'Name', 'Team', dataSource)
+
+# ===========
+# Finalize loaded data
+# ===========
+def finalizeCombinedSet(frames : Dict[DataSource, pd.DataFrame]) -> pd.DataFrame:
+    # Split dfs, and convert dates
+    dfs, ref = frames['dfs'], frames['ref']
+    dfs['Date'] = pd.to_datetime(dfs['Date'])
+    ref['Date'] = pd.to_datetime(ref['Date'])
+
+    # Get lookup names onto dfs
+    lookup = pd.read_csv(f'../data/lookup/name_lookup.csv')
+    dfs = dfs.merge(lookup, left_on = ['Team','Name'], right_on = ['Team_dfs','Name_dfs'], how = 'outer', indicator = True)
+    dfs[['Name','Team']] = dfs[['Player_ref','Team_ref']]
+    # Every dfs player has a ref match
+        #print(dfs['_merge'].value_counts())
+        #print(dfs.head())
+
+    # Merge ref and dfs, return inner join results
+    big = ref.merge(dfs, left_on = ['Team', 'Name', 'Date'], right_on = ['Team_ref', 'Player_ref', 'Date'], how = 'outer', indicator = '_merge2')
+    # 9,000 of 50,000 ref data observations don't have match in dfs
+        #print(big['_merge2'].value_counts())
+        #print(ref.shape)
+    # 20,000 of 50,000 dfs data observations don't have match in ref
+    # Most are injuries / scratches
+
+    both = big[big['_merge2'] == 'both'].copy()
+    return both
+
 def main():
     year_start = 2022
     year_end = 2023
     target_years = set(range(year_start, year_end+1))
 
     # Extract and save yearly data if it doesn't exist
-    frames = {}
-    for dataSource in DataSource:
-        saved_years = {int(re.search(r'_(\d{4})\.csv', i).group(1)) for i in glob.glob(f'../data/import/{dataSource.value}_*.csv')}
-        missing_years = target_years - saved_years
-        for year in missing_years:
-            logger.info(f"Extracting data for {year}...")
-            extractAndSaveYear(year)
-        frames[dataSource.value] = loadSource(dataSource)
-    # Load data
-
+    frames = loadFrames(target_years)
     frames['dfs'], frames['ref'] = standardizeNames(frames['dfs'], frames['ref'])
 
     # Export lookup table helpers if they don't exist
-    for dataSource in DataSource:
-        if not os.path.exists(f'../data/lookup/names_{dataSource.value}.csv'):
-            exportToMakeLookup(frames[dataSource.value], 'Name', 'Team', dataSource)
-    
-    dfs, ref = frames['dfs'], frames['ref']
-    dfs['Date'] = pd.to_datetime(dfs['Date'])
-    ref['Date'] = pd.to_datetime(ref['Date'])
-    print(dfs.sort_values(['Date', 'Proj'], ascending = [True, False]).head(10))
-    print(ref.sort_values(['Date', 'Total Points'], ascending = [True, False]).head(10))
+    exportLookupHelpers(frames)
 
-    lookup = pd.read_csv(f'../data/lookup/name_lookup.csv')
-
-    dfs = dfs.merge(lookup, left_on = ['Team','Name'], right_on = ['Team_dfs','Name_dfs'], how = 'outer', indicator = True)
-    print(dfs['_merge'].value_counts())
-    print(dfs.head())
-
-    dfs[['Name','Team']] = dfs[['Player_ref','Team_ref']]
-
-    print(dfs[(dfs['Name'].isnull()) | (dfs['Team'].isnull())].sort_values('Proj', ascending = False))
-    print(dfs[(dfs['Name'].isnull()) | (dfs['Team'].isnull())].sort_values('Proj', ascending = False).head(50))
-    print(ref[(ref['Date'] == '2022-11-27')] )
-
-    print(ref.head())
-    print(dfs.head())
-    
-    big = ref.merge(dfs, left_on = ['Team', 'Name', 'Date'], right_on = ['Team_ref', 'Player_ref', 'Date'], how = 'outer', indicator = '_merge2')
-    print(big.columns)
-    print(big['_merge2'].value_counts())
-    print(big.loc[big['_merge2'] == 'left_only', ['Team_x','Opp', 'Name_x','Date', 'Total Points']].sort_values('Total Points', ascending = False).head(50))
-
-    # TODO:
-    # Consider, draftkings data only has late-night, x-game slates
-    #   whereas fanduel has seemingly all games
-    # Should we switch to fanduel entirely?
-    # Or just proceed with draftkings and ignore the missing data?
-    
-
-    # print(ref['_merge'].value_counts()) 
-
-    # lookup = lookup.merge(dfs, left_on = ['Team_dfs','Name_dfs'], right_on = ['Team','Name'], how = 'outer', indicator = 'lookup_dfs_merge')
-    # lookup = lookup.merge(ref, left_on = ['Team_ref','Player_ref'], right_on = ['Team','Name'], how = 'outer', indicator = 'lookup_ref_merge')
-    # print(lookup)
-    # a = lookup.merge(dfs, left_on = ['Team_dfs','Name_dfs'], right_on = ['Team','Name'], how = 'outer', indicator = True)
-    # print(a[a['_merge'] == 'right_only'][['Team','Name']])
-    # print(a[a['_merge'] == 'left_only'][['Team_dfs','Name_dfs', 'Team_ref', 'Player_ref']])
-
-    # b = lookup.merge(ref, left_on = ['Team_ref','Player_ref'], right_on = ['Team','Name'], how = 'outer', indicator = True)
-    # print(b[b['_merge'] == 'right_only'][['Team','Name']])
-    # print(b[b['_merge'] == 'left_only'][['Team_dfs','Name_dfs', 'Team_ref', 'Player_ref']])
-
+    bigFrame = finalizeCombinedSet(frames)
+    print(bigFrame.head())
+    print(bigFrame.shape)
 
     
 if __name__ == '__main__':
